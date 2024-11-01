d80cb4035742713854b78df7f46dab7e745eec34
Dhruv K Jain <109206275+dhruvjaink07@users.noreply.github.com>
2024-11-01 21:56:16 +0530

    Update ApiInterface.kt

--- app/src/main/java/com/example/musicapp/ApiInterface.kt
+++ app/src/main/java/com/example/musicapp/ApiInterface.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface ApiInterface {
 
-    @Headers("X-RapidAPI-Key: 9116c8e1ebmsh2e846ef01be3f8bp191d26jsn53b3c2370715",
+    @Headers("X-RapidAPI-Key: xxx",
         "X-RapidAPI-Host: deezerdevs-deezer.p.rapidapi.com")
     @GET("search")
     fun getData(@Query("q") query: String) : Call<MusicData>
